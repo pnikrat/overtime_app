f11677f6307f6d2f075dbe5374683912d77e334f
pnikrat <sniffoza@gmail.com>
2017-08-27 20:43:16 +0200

    Implemented creation process for posts and refactored

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,30 @@
 class PostsController < ApplicationController
+  before_action :get_post, only: %i[show]
+
   def index; end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: 'Your post was created successfully'
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:date, :rationale)
+    end
+
+    def get_post
+      @post = Post.find(params[:id])
+    end
 end
